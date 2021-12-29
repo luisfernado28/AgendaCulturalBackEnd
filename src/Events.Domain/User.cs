3e795c207cf1dbc95eeafe810ebf5e5fa06a8375
Luis Fernando Choque <luisfer_2810@outlook.com>
2021-12-29 15:55:14 -0400

    AB#60 User model updated

--- src/Events.Domain/User.cs
+++ src/Events.Domain/User.cs
@@ -9,10 +9,10 @@ namespace Events.Domain
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Name { get; set; }
-        public int Role{ get; set; }
-        public string Email{ get; set; }
-        public string Password{ get; set; }
-        public string[] Favorites { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public bool Admin { get; set; }
     }
 }
